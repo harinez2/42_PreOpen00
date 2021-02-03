3a658823fd7ebd57a43f1e7ac60e0151b992a4ff
Yoji Onishi <yonishi@c1r15s8.42tokyo.jp>
2021-02-03 21:19:53 +0900

    ex07 fix

--- ex07/periodic_table.py
+++ ex07/periodic_table.py
@@ -6,7 +6,7 @@
 #    By: yonishi <yonishi@student.42tokyo.j>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/02/03 17:52:15 by yonishi           #+#    #+#              #
-#    Updated: 2021/02/03 20:39:23 by yonishi          ###   ########.fr        #
+#    Updated: 2021/02/03 21:17:50 by yonishi          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -24,6 +24,10 @@ html_head = """\
     <meta name="author" content="SitePoint">
 
     <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
     <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
@@ -53,17 +57,19 @@ for line in lines:
     d[num] = { 'name': name, 'position': position, 'short': short, 'mol': mol, 'electron': electron }
 f.close()
 
-data = '<table>\n' 
+data = '<h1 style="text-align: center">THE PERIODIC TABLE</h1>\n' 
+data += '<table class="table table-bordered">\n' 
 idx = 1
 for i in range(7):
     data += '  <tr>\n'
     for j in range(18):
         if int(d[str(idx)]['position']) == j:
-            data += '    <td>' + str(idx) + '<br>' \
-                    + d[str(idx)]['name'] + '<br>' \
-                    + d[str(idx)]['short'] + '<br>' \
-                    + d[str(idx)]['mol'] + '<br>' \
-                    + d[str(idx)]['electron'] + '<br>' \
+            data += '    <td>' \
+                    + '<h5>' + str(idx) + '</h5>' \
+                    + '<h1 style="font-size: 56px">' + d[str(idx)]['short'] + '</h1>' \
+                    + '<p>' + d[str(idx)]['name'] + '<br>' \
+                    + 'mol: ' + d[str(idx)]['mol'] + '<br>' \
+                    + 'elec.: ' + d[str(idx)]['electron'] + '</p>' \
                     + '</td>\n'
             if idx == 56:
                 idx = 72
