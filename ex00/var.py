dec5e40963c0ffe705f6c8f28848cc36b72ed03c
Yoji Onishi <yonishi@c1r15s8.42tokyo.jp>
2021-02-03 14:53:10 +0900

    ex00 fix

--- ex00/var.py
+++ ex00/var.py
@@ -6,31 +6,18 @@
 #    By: yonishi <yonishi@student.42tokyo.j>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/02/03 13:47:12 by yonishi           #+#    #+#              #
-#    Updated: 2021/02/03 14:45:12 by yonishi          ###   ########.fr        #
+#    Updated: 2021/02/03 14:52:49 by yonishi          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-num = 42
-print('%d is type of %s' % (num, type(num)))
+def my_var(num, str1, str2, fl, lst, dct, tpl, setdata):
+    print('%d is type of %s' % (num, type(num)))
+    print('%s is type of %s' % (str1, type(str1)))
+    print('%s is type of %s' % (str2, type(str2)))
+    print('%.1f is type of %s' % (fl, type(fl)))
+    print('%s is type of %s' % (lst, type(lst)))
+    print('%s is type of %s' % (dct, type(dct)))
+    print('%s is type of %s' % (tpl, type(tpl)))
+    print('%s is type of %s' % (setdata, type(setdata)))
 
-s = '42'
-print('%s is type of %s' % (s, type(s)))
-
-str2 = 'quarante-deux'
-print('%s is type of %s' % (str2, type(str2)))
-
-fl = 42.0
-print('%.1f is type of %s' % (fl, type(fl)))
-
-lst = [42, 42]
-print('%s is type of %s' % (lst, type(lst)))
-
-dct = {42: 42}
-print('%s is type of %s' % (dct, type(dct)))
-
-tpl = (42, )
-print('%s is type of %s' % (tpl, type(tpl)))
-
-set = set()
-print('%s is type of %s' % (set, type(set)))
-
+my_var(42, '42', 'quarante-deux', 42.0, [42, 42], {42: 42}, (42, ), set())
